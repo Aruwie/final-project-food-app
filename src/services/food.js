478c96c2fb82da938ae9a7bdc9ee3e8abdd9ae96
Aruwie <83230807+Aruwie@users.noreply.github.com>
2026-07-01 04:04:05 +0700

    Fixing Update Food

--- src/services/food.js
+++ src/services/food.js
@@ -21,7 +21,7 @@ export async function createFood(foodData) {
 // UPDATE FOOD
 export async function updateFood(id, foodData) {
   return fetchAPI(`/update-food/${id}`, {
-    method: "PUT", // lebih standar daripada POST
+    method: "POST",
     body: JSON.stringify(foodData),
   });
 }
